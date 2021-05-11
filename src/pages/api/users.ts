43ebb4f1e92ab7bd222fd92d9a1c2dff7cd15600
bruno-nakahara <Bruno_Nakahara95@yahoo.com>
2021-05-11 12:23:17 -0300

    API routes users

---
+++ src/pages/api/users.ts
@@ -0,0 +1,11 @@
+import { NextApiRequest, NextApiResponse} from 'next';
+
+export default (request: NextApiRequest, response: NextApiResponse) => {
+    const users = [
+        { id: 1, name: 'Bruno' },
+        { id: 2, name: 'Diego' },
+        { id: 3, name: 'Mayk' },
+    ]
+
+    return response.json(users)
+}
