c920a139dc6708bcc486310784ee713995b255fa
bruno-nakahara <Bruno_Nakahara95@yahoo.com>
2021-05-13 18:20:22 -0300

    ActiveLink

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
+import { ActiveLink } from '../ActiveLink';
 
 export function Header() {
     return (
@@ -7,8 +8,13 @@ export function Header() {
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="Logo" />
                 <nav>
-                    <a href="#" className={styles.active}>Home</a>
-                    <a href="#">Post</a>
+                    <ActiveLink activeClassName={styles.active} href="/">
+                        <a>Home</a>
+                    </ActiveLink>
+
+                    <ActiveLink activeClassName={styles.active} href="/posts">
+                        <a>Post</a>
+                    </ActiveLink>
                 </nav>
 
                 <SignInButton />
