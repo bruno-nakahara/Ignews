6c1658dfeb926ec6d2d4516aef9651cf38c50daf
bruno-nakahara <Bruno_Nakahara95@yahoo.com>
2021-06-22 11:27:54 -0300

    test

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -46,6 +46,16 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         if (relevantEvents.has(type)) {
             try {
                 switch (type) {
+                    case 'checkout.session.completed':
+                        const checkoutSession = event.data.object as Stripe.Checkout.Session;
+
+                        await saveSubscription(
+                            checkoutSession.subscription.toString(), 
+                            checkoutSession.customer.toString(),
+                            true,
+                        )
+                        
+                        break;
                     case 'customer.subscription.updated':
                     case 'customer.subscription.deleted':
 
@@ -57,16 +67,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
                             false,
                         )
 
-                        break;
-                    case 'checkout.session.completed':
-                        const checkoutSession = event.data.object as Stripe.Checkout.Session;
-
-                        await saveSubscription(
-                            checkoutSession.subscription.toString(), 
-                            checkoutSession.customer.toString(),
-                            true,
-                        )
-                        
                         break;
                     default:
                         throw new Error("Unhandle event");              
